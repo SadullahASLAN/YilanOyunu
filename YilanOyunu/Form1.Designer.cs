1bb620cc0ce8ac1e6925c84d2781b48bad79fb79
Sadullah ASLAN <sadullahaslan@hotmail.com>
2019-12-04 00:28:07 +0300

    Add project files.

---
+++ YilanOyunu/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace YilanOyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.yilan1 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.yilan2 = new System.Windows.Forms.Label();
+            this.yilan3 = new System.Windows.Forms.Label();
+            this.lblSkor = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // yilan1
+            // 
+            this.yilan1.BackColor = System.Drawing.Color.White;
+            this.yilan1.Location = new System.Drawing.Point(320, 320);
+            this.yilan1.Name = "yilan1";
+            this.yilan1.Size = new System.Drawing.Size(19, 19);
+            this.yilan1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.DarkRed;
+            this.label1.Location = new System.Drawing.Point(667, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "X";
+            this.label1.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // yilan2
+            // 
+            this.yilan2.BackColor = System.Drawing.Color.White;
+            this.yilan2.Location = new System.Drawing.Point(300, 320);
+            this.yilan2.Name = "yilan2";
+            this.yilan2.Size = new System.Drawing.Size(19, 19);
+            this.yilan2.TabIndex = 2;
+            // 
+            // yilan3
+            // 
+            this.yilan3.BackColor = System.Drawing.Color.White;
+            this.yilan3.Location = new System.Drawing.Point(280, 320);
+            this.yilan3.Name = "yilan3";
+            this.yilan3.Size = new System.Drawing.Size(19, 19);
+            this.yilan3.TabIndex = 3;
+            // 
+            // lblSkor
+            // 
+            this.lblSkor.AutoSize = true;
+            this.lblSkor.ForeColor = System.Drawing.Color.White;
+            this.lblSkor.Location = new System.Drawing.Point(12, 9);
+            this.lblSkor.Name = "lblSkor";
+            this.lblSkor.Size = new System.Drawing.Size(13, 13);
+            this.lblSkor.TabIndex = 4;
+            this.lblSkor.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(700, 700);
+            this.Controls.Add(this.lblSkor);
+            this.Controls.Add(this.yilan3);
+            this.Controls.Add(this.yilan2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.yilan1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label yilan1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label yilan2;
+        private System.Windows.Forms.Label yilan3;
+        private System.Windows.Forms.Label lblSkor;
+    }
+}
+
---
+++ YilanOyunu/Form1.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YilanOyunu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        Label[] Yilan = new Label[3];
+        Point[] konumlar = new Point[3];
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            if (yukari)
+            {
+                yilan1.Location = new Point(yilan1.Location.X, yilan1.Location.Y - 20);
+                if (yilan1.Location.Y < 0)
+                {
+                    yilan1.Location = new Point(yilan1.Location.X, this.Height - 20);
+                }
+            }
+            else if (asagi)
+            {
+                yilan1.Location = new Point(yilan1.Location.X, yilan1.Location.Y + 20);
+                if (yilan1.Location.Y > this.Height - 20)
+                {
+                    yilan1.Location = new Point(yilan1.Location.X, 0);
+                }
+            }
+            else if (sag)
+            {
+                yilan1.Location = new Point(yilan1.Location.X + 20, yilan1.Location.Y);
+                if (yilan1.Location.X > this.Width - 20)
+                {
+                    yilan1.Location = new Point(0, yilan1.Location.Y);
+                }
+            }
+            else if (sol)
+            {
+                yilan1.Location = new Point(yilan1.Location.X - 20, yilan1.Location.Y);
+                if (yilan1.Location.X < 0)
+                {
+                    yilan1.Location = new Point(this.Width - 20, yilan1.Location.Y);
+                }
+            }
+
+            for (int i = 0; i < Yilan.Length; i++)
+            {
+                if (i < Yilan.Length - 1)
+                    Yilan[i + 1].Location = konumlar[i];
+                konumlar[i] = Yilan[i].Location;
+            }
+
+            if (Yilan[0].Location == lbl.Location)
+            {
+                ElmaOlustur();
+
+                Label lbl = new Label();
+                lbl.Width = lbl.Height = 19;
+                lbl.BackColor = Color.White;
+                lbl.Location = konumlar[konumlar.Length - 1];
+                Array.Resize(ref Yilan, Yilan.Length + 1);
+                Yilan[Yilan.Length - 1] = lbl;
+                Array.Resize(ref konumlar, konumlar.Length + 1);
+                konumlar[konumlar.Length - 1] = lbl.Location;
+                this.Controls.Add(lbl);
+                skor++;
+                lblSkor.Text = skor.ToString();
+            }
+
+            for (int i = 1; i < konumlar.Length; i++)
+            {
+                if (Yilan[0].Location == konumlar[i] && skor != 0)
+                {
+                    timer1.Stop();
+                    MessageBox.Show("Yandın.", "Oyun Bitti", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    this.Close();
+                }
+            }
+        }
+
+        int skor = 0;
+
+        bool yukari, asagi, sag, sol;
+
+        Random rnd = new Random();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Yilan[0] = yilan1;
+            Yilan[1] = yilan2;
+            Yilan[2] = yilan3;
+
+            konumlar[0] = yilan1.Location;
+            konumlar[1] = yilan2.Location;
+            konumlar[2] = yilan3.Location;
+
+            ElmaOlustur();
+        }
+
+        Label lbl;
+
+        void ElmaOlustur()
+        {
+            if (lbl != null)
+            {
+                lbl.Dispose();
+            }
+            lbl = new Label();
+            lbl.Width = lbl.Height = 19;
+            lbl.BackColor = Color.Red;
+            lbl.Location = new Point(rnd.Next(35) * 20, rnd.Next(35) * 20);
+            atla:
+            foreach (var item in konumlar)
+            {
+                if (item == lbl.Location)
+                {
+                    lbl.Location = new Point(rnd.Next(35) * 20, rnd.Next(35) * 20);
+                    goto atla;
+                }
+            }
+            this.Controls.Add(lbl);
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (!timer1.Enabled)
+            {
+                timer1.Start();
+                sag = true;
+            }
+            switch (e.KeyCode)
+            {
+                case Keys.Left:
+                    if (!sag)
+                    {
+                        asagi = false;
+                        yukari = false;
+                        sag = false;
+                        sol = true;
+                    }
+                    break;
+                case Keys.Up:
+                    if (!asagi)
+                    {
+                        asagi = false;
+                        yukari = true;
+                        sag = false;
+                        sol = false;
+                    }
+                    break;
+                case Keys.Right:
+                    if (!sol)
+                    {
+                        asagi = false;
+                        yukari = false;
+                        sag = true;
+                        sol = false;
+                    }
+                    break;
+                case Keys.Down:
+                    if (!yukari)
+                    {
+                        asagi = true;
+                        yukari = false;
+                        sag = false;
+                        sol = false;
+                    }
+                    break;
+            }
+        }
+    }
+}
